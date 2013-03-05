3bc7c70a014b9a2aeb84ae16f9595e4897e5b0da
Sander Nijhof <sander1992nijhof@gmail.com>
2013-03-05 22:29:16 +0100

    -Client cleaned

--- client/element53/gen/biz/nijhof/e53/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package biz.nijhof.e53;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- client/element53/server/config53.js
+++
@@ -1,9 +0,0 @@
-exports.config =
-{
-	domain: 't.nijhof.biz',
-	port: 53,
-	server_ip: 'localhost',
-	server_port: 1080,
-	max_client_inactive_ms: 5 * 60 * 1000,
-	status_interval_ms: 3000
-}
